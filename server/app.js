9b5a8ea869b73ab4ae47df653c1f356355680324
yejin <kyj1860@naver.com>
2022-06-13 11:58:37 +0900

    login, join sql 추가

--- server/app.js
+++ server/app.js
@@ -7,13 +7,11 @@ const bodyParser = require('body-parser');
 
 const router = require('./routes/route')
 
-app.use(router)
-
 // app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/../views')
 app.use(express.json())
-app.use(express.urlencoded({extended : false }))
+app.use(express.urlencoded({extended : true }))
 app.use(session({
     secret: '123',
     resave: true,
@@ -21,6 +19,7 @@ app.use(session({
 }))
 app.use(express.static(__dirname + '/../views'))
 
+app.use(router)
 app.use('/css', express.static(path.resolve(__dirname, "../assets/css")))
 app.use('/images', express.static(path.resolve(__dirname, "../assets/images")))
 app.use('/js', express.static(path.resolve(__dirname, "../assets/js")))
--- server/routes/join.js
+++ server/routes/join.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('join')
 })
 
+router.post('/', (req, res) => {
+    var user = [req.body.id, req.body.pw, req.body.name, req.body.phone]
+    db.query('insert into user(user_ID, password, name, phone) values (?, ?, ?, ?)', user, (err, result) => {
+        if (err) throw err
+        res.redirect('/login')
+    })
+})
+
 module.exports = router
