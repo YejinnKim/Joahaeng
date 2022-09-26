3dd4c75402614b0384ef3a744a2ee92de5d5776f
YejinnKim <kyj1860@naver.com>
2022-09-26 14:20:28 +0900

    검색기능 수정

--- routes/mytrip.js
+++ routes/mytrip.js
@@ -5,20 +5,36 @@ const db = require('./db')
 
 router.get('/', (req, res) => {
     let user = req.session.user
+    let url = 'http://apis.data.go.kr/B551011/KorService/areaBasedList'
+    url += `?ServiceKey=${process.env.APIKEY}`
+    url += `&MobileOS=ETC`
+    url += `&MobileApp=AppTest`
+    url += `&_type=json`
+    url += `&numOfRows=36602`
     if (!user) res.redirect('/login')
     else {
         var id
         if (user) id = user.id
+        var arr = []
         var board_sql = `SELECT * FROM board WHERE user_ID='${id}' ORDER BY board_ID DESC`
         var image_sql = `SELECT board_ID, filename FROM image`
         var mytrip_sql = `SELECT * FROM mytrip WHERE user_ID='${id}'`
+        
         db.query(board_sql, (err, board_result) => {
             if (err) throw err
             db.query(image_sql, (err, image_result) => {
                 if (err) throw err
                 db.query(mytrip_sql, (err, mytrip_result) => {
                     if (err) throw err
-                    res.render('mytrip', {page: '나의 여행', user: user, boardData: board_result, image: image_result, data: mytrip_result})
+                    request (
+                        {url: url, method: 'GET'}, (error, response, body) => {
+                            dataList = JSON.parse(body).response.body.items.item
+                            mytrip_result.map(element => {
+                                arr.push(dataList.find(e => e.contentid == element.contentid))
+                            })
+                            res.render('mytrip', {page: '나의 여행', user: user, boardData: board_result, image: image_result, mytripData: arr})
+                        }
+                    )
                 })
             })
         })
--- routes/place.js
+++ routes/place.js
@@ -10,12 +10,12 @@ router.get('/', (req, res) => {
     url += `&MobileOS=ETC`
     url += `&MobileApp=AppTest`
     url += `&_type=json`
-    url += `&numOfRows=100`
+    url += `&numOfRows=200`
     url += `&arrange=P`
     if (req.query.area)
         url += `&areaCode=${req.query.area}`
 
-    request(
+    request (
         {url: url, method: 'GET'}, (error, response, body) => {
             dataList = JSON.parse(body).response.body.items.item
             res.render('place', {page: '여행지 찾기', user: user, data: dataList})
@@ -26,23 +26,30 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
     var dataList
+    let url
     let user = req.session.user
-    let url = '	http://apis.data.go.kr/B551011/KorService/searchKeyword'
+    let keyword = urlencode(req.query.keyword)
+    let theme = req.query.theme
+    let area = req.query.area
+
+    if (keyword)
+        url = 'http://apis.data.go.kr/B551011/KorService/searchKeyword'
+    else 
+        url = 'http://apis.data.go.kr/B551011/KorService/areaBasedList'
     url += `?ServiceKey=${process.env.APIKEY}`
     url += `&MobileOS=ETC`
     url += `&MobileApp=AppTest`
     url += `&_type=json`
     url += `&numOfRows=100`
     url += `&arrange=P`
-    url += `&keyword=`
-    if (req.query.keyword) 
-        url += `&keyword=${urlencode(req.query.keyword)}`
-    if (req.query.theme) 
-        url += `&cat1=${urlencode(req.query.theme)}`
-    if (req.query.area) 
-        url += `&areaCode=${urlencode(req.query.area)}`
+    if (keyword)
+        url += `&keyword=${keyword}`
+    if (theme)
+        url += `&cat1=${theme}`
+    if (area)
+        url += `&areaCode=${area}`
     
-    request(
+    request (
         {url: url, method: 'GET'}, (error, response, body) => {
             dataList = JSON.parse(body).response.body.items.item
             res.render('place', {page: '여행지 찾기', user: user, data: dataList})
@@ -52,7 +59,24 @@ router.get('/search', (req, res) => {
 
 router.get('/detail', (req, res) => {
     let user = req.session.user
-    res.render('place_detail', {page: '여행지 정보', user: user})
+    let url = 'http://apis.data.go.kr/B551011/KorService/detailCommon'
+    url += `?ServiceKey=${process.env.APIKEY}`
+    url += `&MobileOS=ETC`
+    url += `&MobileApp=AppTest`
+    url += `&_type=json`
+    url += `&contentId=${req.query.contentid}`
+    url += `&defaultYN=Y`
+    url += `&firstImageYN=Y`
+    url += `&addrinfoYN=Y`
+    url += `&mapinfoYN=Y`
+
+    request (
+        {url: url, method: 'GET'}, (error, response, body) => {
+            data = JSON.parse(body).response.body.items.item[0]
+            res.render('place_detail', {page: '여행지 정보', user: user, data: data})
+        }
+    )
+    
 })
 
 module.exports = router
